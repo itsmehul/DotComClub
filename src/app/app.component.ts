65ac36646d2d9bf04dae13dda4c6c1727f8f44a2
mrgawde <mehulgawdenxt@gmail.com>
2018-02-03 08:30:07 +0530

    feat: Final touches to responsive navigation \n Changed css. Added side menu text.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,19 @@ export class AppComponent {
         $(this).toggleClass('open');
         if ($('.sidebar').width() == 0) {
           $('.sidebar').animate({ 'width': '80vw' }, '100ms', 'swing');
+          $('.smenu').css('visibility','visible');
         }
         else {
           $('.sidebar').animate({ 'width': '0vw' }, '100ms', 'swing');
+          $('.smenu').css('visibility','hidden');
         }
 
       });
+      $('.btn>button.mat-button').click(function () {
+        console.log($('button.mat-button'));
+        $('button.mat-button').css('background', '');
+        $(this).css('background', 'rgb(0, 56, 0)');
+      })
 
     });
   }
