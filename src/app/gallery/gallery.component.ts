4284855cc9f493fb4a7321f2692cc533551e81dd
mrgawde <mehulgawdenxt@gmail.com>
2018-02-03 23:52:54 +0530

    feat: Gallery component now displays images from firestore database

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Img{
+  name: string;
+  url: string;    
+  }
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +15,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  imgCol: AngularFirestoreCollection<Img>;
+  imgs: any;
+
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.imgCol = this.afs.collection('gallery');
+    console.log(this.imgCol);
+    this.imgs = this.imgCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Img;
+          const id = a.payload.doc.id;
+          console.log(id);
+          return { id, data };
+        });
+      });
+    console.log(this.imgs);
   }
 
 }
