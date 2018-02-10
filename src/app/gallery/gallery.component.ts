358b35a803399922f4c6b70e66d501bceec0f82b
mrgawde <mehulgawdenxt@gmail.com>
2018-02-10 23:18:52 +0530

    refractor: Gallery component Cleaned up unused code and injected the Gallery Service

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import {GalleryService} from '../services/gallery.service';
+import {Img} from '../models/gallery';
 
-interface Img{
-  name: string;
-  url: string;    
-  }
+
 
 @Component({
   selector: 'app-gallery',
@@ -14,25 +10,30 @@ interface Img{
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  imgs: Img[];
+  img: Img = {
+    name: '',
+    url: ''
+  }
 
-  imgCol: AngularFirestoreCollection<Img>;
-  imgs: any;
-
-  constructor(private afs: AngularFirestore) { }
+  constructor(public galleryService: GalleryService) { }
 
   ngOnInit() {
-    this.imgCol = this.afs.collection('gallery');
-    console.log(this.imgCol);
-    this.imgs = this.imgCol.snapshotChanges()
-      .map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data() as Img;
-          const id = a.payload.doc.id;
-          console.log(id);
-          return { id, data };
-        });
-      });
-    console.log(this.imgs);
+    this.galleryService.getImgs().subscribe(data=>{
+      this.imgs=data;
+      console.log(data);
+    })
+
+  }
+  onSubmit(){
+    if(this.img.name!='' && this.img.url!=''){
+      this.galleryService.addImg(this.img);
+      this.img.name='';
+      this.img.url='';
+    }
+  }
+  deleteimg(event,img){
+    this.galleryService.deleteImg(img);
   }
 
 }
